9237014384fb58ef3f62fbab903662c11c342863
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-23 19:05:40 +0330

    folders and services classes was added.

---
+++ Sources/02. Services/RoyalVilla.Services.ApplicationServices/Villas/VillasService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalVilla.Services.ApplicationServices.Villas;
+
+public class VillasService
+{
+}
---
+++ Sources/02. Services/RoyalVilla.Services.ApplicationServices/VillasNumbers/VillasNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalVilla.Services.ApplicationServices.VillasNumbers;
+
+public class VillasNumbers
+{
+}
