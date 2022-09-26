01d7482d213bcb27c2d122ff405d279cc21e245c
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-26 22:49:49 +0330

    RoyalVilla.Endpoints.VillaWeb was created.

---
+++ Sources/04. Endpoints/RoyalVilla.Endpoints.VillaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoyalVilla.Endpoints.VillaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
