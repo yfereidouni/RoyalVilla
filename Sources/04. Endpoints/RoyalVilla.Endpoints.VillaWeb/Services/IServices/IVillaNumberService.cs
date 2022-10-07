591e38685ef80f01a0bb5c25697a1f1ce3315dd8
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-07 10:34:29 +0330

    VillaNumberService and Interface was developed.

---
+++ Sources/04. Endpoints/RoyalVilla.Endpoints.VillaWeb/Services/IServices/IVillaNumberService.cs
@@ -0,0 +1,12 @@
+﻿using RoyalVilla.Endpoints.VillaWeb.Models.DTOs;
+
+namespace RoyalVilla.Endpoints.VillaWeb.Services.IServices;
+
+public interface IVillaNumberService
+{
+    Task<T> GetAllAsync<T>();
+    Task<T> GetAsync<T>(int id);
+    Task<T> CreateAsync<T>(VillaNumberCreateDTO dto);
+    Task<T> UpdateAsync<T>(VillaNumberUpdateDTO dto);
+    Task<T> DeleteAsync<T>(int id);
+}
