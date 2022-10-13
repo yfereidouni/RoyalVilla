0ca8dc9493fea18d4ea6b5961fa664574660686f
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-10-14 00:37:52 +0330

    ok

--- Sources/01. Core/RoyalVilla.Core.Contracts/LocalUsers/ILocalUserRepository.cs
+++ Sources/01. Core/RoyalVilla.Core.Contracts/LocalUsers/ILocalUserRepository.cs
@@ -1,9 +1,12 @@
 ﻿using RoyalVilla.Core.Contracts.Common;
 using RoyalVilla.Core.Entities.LocalUsers;
+using System.Runtime.CompilerServices;
 
 namespace RoyalVilla.Core.Contracts.LocalUsers;
 
 public interface ILocalUserRepository : IBaseRepository<LocalUser>
 {
     Task<LocalUser> UpdateAsync(LocalUser entity);
+    //Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+    //public bool IsUniqueUser { get; set; }
 }
